e49148240e716f4334693e134e38d671770b7887
chaitu540 <112631033+chaitu540@users.noreply.github.com>
2022-09-05 14:47:18 +0530

    Add files via upload

---
+++ given key already exists in a dictionary..py
@@ -0,0 +1,17 @@
+# Python3 Program to check whether a
+# given key already exists in a dictionary.
+
+def checkKey(dic, key):
+	if key in dic.keys():
+		print("Present, ", end =" ")
+		print("value =", dic[key])
+	else:
+		print("Not present")
+		
+# Driver Code
+dic = {'a': 100, 'b':200, 'c':300}
+key = 'b'
+checkKey(dic, key)
+
+key = 'w'
+checkKey(dic, key)
