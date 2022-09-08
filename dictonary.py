526977c66952d68af1f776ba52c77dfd915c3a96
chaitu540 <112631033+chaitu540@users.noreply.github.com>
2022-09-08 13:16:38 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,9 @@
+my_dict_1 = {'J':12,'W':22}
+my_dict_2 = {'M':67}
+print("The first dictionary is :")
+print(my_dict_1)
+print("The second dictionary is :")
+print(my_dict_2)
+my_dict_1.update(my_dict_2)
+print("The concatenated dictionary is :")
+print(my_dict_1)
